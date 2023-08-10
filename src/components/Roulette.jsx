15071aac98d896f200b867cab5fd7642e2643327
Shohjahon <104879567+mr-webprogrammer@users.noreply.github.com>
2023-08-10 14:14:57 +0500

    add roulette

--- src/components/Roulette.jsx
+++ src/components/Roulette.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Wheel } from 'react-custom-roulette'
+
+const data = [
+    { option: '0' },
+    { option: '1' },
+    { option: '2' },
+]
 
 const Roulette = () => {
-  return (
-    <div>Roulette</div>
-  )
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
+
+    const handleSpinClick = () => {
+        if (!mustSpin) {
+            const newPrizeNumber = Math.floor(Math.random() * data.length);
+            setPrizeNumber(newPrizeNumber);
+            setMustSpin(true);
+        }
+    }
+
+    return (
+        <div>
+            <Wheel
+                mustStartSpinning={mustSpin}
+                prizeNumber={prizeNumber}
+                data={data}
+
+                onStopSpinning={() => {
+                    setMustSpin(false);
+                }}
+            />
+            <button onClick={handleSpinClick}>SPIN</button>
+        </div>
+    )
 }
 
 export default Roulette
